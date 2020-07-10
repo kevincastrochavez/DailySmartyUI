28938f061a16d51f68dbd46453db19183c3d7e91
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-10 18:59:08 -0500

    added exact route for home route and implemented withRouter HOC into the searchbar to navigate the user to results route

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,13 @@
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm } from 'redux-form';
 import React, { Component } from 'react';
+
+import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
         console.log("handle query", query);
+        this.props.history.push('/results');
     }
 
     renderInput(field) {
@@ -25,5 +28,7 @@ class SearchBar extends Component {
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
