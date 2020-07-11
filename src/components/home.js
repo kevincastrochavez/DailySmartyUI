ae86671a6cbcbc70892462074a0a32390bc31461
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-10 22:30:08 -0500

    modified SearchBar Component to take onSubmit function via props

--- src/components/home.js
+++ src/components/home.js
@@ -4,12 +4,18 @@ import SearchBar from "./searchBar";
 import RecentPosts from "./recentPosts";
 
 export default class Home extends Component {
+
+  handleSearchBarSubmit(query) {
+    console.log("handle query", query);
+    this.props.history.push('/results');
+  }
+  
   render() {
     return (
       <div className='app'>
         <div>
           <Logo/>
-          <SearchBar/>
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
           <RecentPosts/>
         </div>
       </div>
