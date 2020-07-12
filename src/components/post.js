899d03ed5a2630a8a53fa6c0f47b1af930127ebf
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-11 23:26:55 -0500

    modified post component to render a result post and setup render links

--- src/components/post.js
+++ src/components/post.js
@@ -10,18 +10,48 @@ class Post extends Component {
         return topics;
     }
 
-    render() {
-        return (
-            <li className="recent-post">
-                <div className="recent-post-title">
-                    {this.props.title}
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                    <div className="post-link-box"></div>
+                    <div className="post-link-link">
+                        <a href={post_link.link_url}>Useful link #{index + 1}</a>
+                    </div>
                 </div>
+            )
+        })
+        return links;
+    }
 
-                <div className="recent-post-topics">
-                    {this.renderTopics()}
-                </div>
-            </li>
-        )
+    render() {
+        if (this.props.type === 'recent') {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post-title">
+                        {this.props.title}
+                    </div>
+
+                    <div className="recent-post-topics">
+                        {this.renderTopics()}
+                    </div>
+                </li>
+            )
+        } else if (this.props.type === 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post-topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post-title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post-links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
     }
 }
 
